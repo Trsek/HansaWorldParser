a409a34e9a6b694a62c6dcbaaad49fe19472a2b1
Zdeno Sekerák <etrsek@gmail.com>
2019-10-18 23:12:11 +0200

    Added file with special functions WindowsTagTools.txt

--- hwp_gener.bat
+++ hwp_gener.bat
@@ -10,5 +10,6 @@ HansaWorldParser.exe -s"D:\HansaWorld\Zdrojovy kod" -g
 copy global.txt global.hansa
 
 HansaWorldParser.exe -s"D:\HansaWorld\Patince_Devel_Small_85190605" -g
-copy global.txt global.patince
+type WindowsTagTools.txt >> global.txt
 type global.hansa >> global.txt
+del global.hansa
