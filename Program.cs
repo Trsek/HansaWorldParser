162f101aef9048f52488de0401faa242217f3a1a
Zdeno Sekerák <etrsek@gmail.com>
2020-08-14 14:46:13 +0200

    Refaktoring using.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Collections.Generic;
-using System.Data.SqlTypes;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 
 namespace HansaWorldParser
